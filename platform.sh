e61c19dd57cc8be4fb0a87801b9e7e75035b62e1
Igor Sarcevic <igisar@gmail.com>
2014-05-08 11:59:18 +0200

    Show ruby versions.

--- platform.sh
+++ platform.sh
@@ -9,3 +9,13 @@ echo `firefox --version`
 echo "\n** Databases:"
 echo `mysql --version`
 echo `psql --version`
+
+echo "\n** Ruby versions:"
+echo `rbenv global 2.1.1 && ruby --version`
+echo `rbenv global 2.1.0 && ruby --version`
+echo `rbenv global 2.0.0 && ruby --version`
+echo `rbenv global 1.9.3 && ruby --version`
+echo `rbenv global 1.9.2 && ruby --version`
+echo `rbenv global 1.8.7 && ruby --version`
+echo `rbenv global jruby-1.7 && ruby --version`
+
